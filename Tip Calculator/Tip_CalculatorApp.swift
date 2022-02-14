6a3db041b985d9837a801522ec759bf9dcc6b9ee
novikovalexey88 <novikov.a.m@yandex.ru>
2022-02-14 12:19:30 +0300

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by 668 on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
